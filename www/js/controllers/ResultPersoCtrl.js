036ba6f4f792d1bd02787b036f447197a4fb280f
paco <pakokrew@gmail.com>
2015-07-29 14:56:43 +0200

    Affichage score avec barres

--- www/js/controllers/ResultPersoCtrl.js
+++ www/js/controllers/ResultPersoCtrl.js
@@ -4,8 +4,40 @@ application.controller('ResultPersoCtrl', function($scope, ResultStore) {
         $scope.resultScore = ResultStore.myScore();
     };
 
-    $scope.$on("update.myscore", updateMyScore)
+    $scope.$on("update.myscore", updateMyScore);
 
     updateMyScore();
 
+    $scope.getBarStyle = function(barScore, orientation) {
+        var style = {};
+
+        if(orientation * barScore.score > 0) {
+            style.width = Math.abs(barScore.score) + '%';
+        }
+
+        return style;
+    };
+
+    var computeBarScores = function() {
+        $scope.barScores = [
+            {
+                name: 'Intro',
+                score: Math.round($scope.resultScore.i * 100)
+            },
+            {
+                name: 'Neutrino',
+                score: Math.round($scope.resultScore.n * 100)
+            },
+            {
+                name: 'Terato',
+                score: Math.round($scope.resultScore.t * 100)
+            },
+            {
+                name: 'Peloto',
+                score: Math.round($scope.resultScore.p * 100)
+            }
+        ]
+    };
+
+    computeBarScores();
 });
