2bc5750e4a054d86de10eb4f20af511fe81473ca
paco <pakokrew@gmail.com>
2015-07-29 14:56:13 +0200

    Route par default

--- www/js/config/router.js
+++ www/js/config/router.js
@@ -50,6 +50,6 @@ application.config(function($stateProvider, $urlRouterProvider) {
         })*/;
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/dash');
+    $urlRouterProvider.otherwise('/tab/test');
 
 });
