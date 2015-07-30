9e4656b1d59055f93cfb6bc4479351d0e9f74953
paco <pakokrew@gmail.com>
2015-07-30 15:50:41 +0200

    Update data questions with json generator

--- www/js/MyersData.js
+++ www/js/MyersData.js
@@ -1,45 +1,114 @@
+// http://beta.json-generator.com/VkTw9Mm9
+
 var MYERS_Questionary = [
     {
-        id: 0,
-        type: 'type1',
-        question: 'Are you mad?',
-        responses: {
-            i: -1,
-            n: 0,
-            t: +1,
-            p: 0
+        "id": 0,
+        "type": "type0",
+        "question": "labore cillum dolore tempor en ?",
+        "responses": {
+            "i": 0,
+            "n": 0,
+            "t": 0,
+            "p": 1
         }
     },
     {
-        id: 1,
-        type: 'type2',
-        question: 'Are you smart?',
-        responses: {
-            i: 1,
-            n: 0,
-            t: 0,
-            p: 1
+        "id": 1,
+        "type": "type1",
+        "question": "ad quis aute pariatur consectet ?",
+        "responses": {
+            "i": 0,
+            "n": 0,
+            "t": 1,
+            "p": 1
         }
     },
     {
-        id: 2,
-        type: 'type3',
-        question: 'Are you ugly?',
-        responses: {
-            i: -1,
-            n: 0,
-            t: 0,
-            p: 1
+        "id": 2,
+        "type": "type2",
+        "question": "cupidatat labore pariatur ad cupidat ?",
+        "responses": {
+            "i": 0,
+            "n": 0,
+            "t": "-1",
+            "p": 0
         }
     },
     {
-        id: 3,
-        type: 'type4',
-        question: 'Are you sad?',
-        responses: {
-            i: -1,
-            n: 0,
-            t: +1,
-            p: 0
+        "id": 3,
+        "type": "type3",
+        "question": "aute ad qui aute deseru ?",
+        "responses": {
+            "i": 0,
+            "n": "-1",
+            "t": 0,
+            "p": 0
         }
-    }];
+    },
+    {
+        "id": 4,
+        "type": "type4",
+        "question": "laboris eu sit sit proide ?",
+        "responses": {
+            "i": "-1",
+            "n": "-1",
+            "t": 0,
+            "p": "-1"
+        }
+    },
+    {
+        "id": 5,
+        "type": "type5",
+        "question": "id enim enim sint q ?",
+        "responses": {
+            "i": "-1",
+            "n": 1,
+            "t": 0,
+            "p": 1
+        }
+    },
+    {
+        "id": 6,
+        "type": "type6",
+        "question": "in sint qui qui  ?",
+        "responses": {
+            "i": 0,
+            "n": 1,
+            "t": "-1",
+            "p": "-1"
+        }
+    },
+    {
+        "id": 7,
+        "type": "type7",
+        "question": "reprehenderit laboris in non du ?",
+        "responses": {
+            "i": 1,
+            "n": 0,
+            "t": 1,
+            "p": 0
+        }
+    },
+    {
+        "id": 8,
+        "type": "type8",
+        "question": "incididunt adipisicing incididunt id min ?",
+        "responses": {
+            "i": "-1",
+            "n": 1,
+            "t": "-1",
+            "p": "-1"
+        }
+    },
+    {
+        "id": 9,
+        "type": "type9",
+        "question": "in ut culpa minim adipisici ?",
+        "responses": {
+            "i": 1,
+            "n": 1,
+            "t": 1,
+            "p": 1
+        }
+    }
+];
